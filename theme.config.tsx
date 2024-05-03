c7ab3ce3cdbd57b2eb68e5cba37a554ac240acc6
Mrlion <mrlion4202@gmail.com>
2024-05-03 14:41:15 +0000

    větší úpravy v theme configu, nové linky atd.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s",
+    };
+  },
+  navigation: {
+    prev: false,
+    next: false,
+  },
+  primaryHue: 222,
+  primarySaturation: 52,
+  docsRepositoryBase: "https://github.com/shuding/nextra/tree/main/docs",
   logo: <span>🦁 Lion Police Roleplay™</span>,
   project: {
-    link: 'https://github.com/L0stedMrlion/lionpolicerp-guide',
+    link: "https://github.com/L0stedMrlion/lionpolicerp-guide",
   },
   chat: {
-    link: 'https://discord.gg/Uewwf3tpz3',
+    link: "https://discord.gg/Uewwf3tpz3",
   },
   footer: {
     text: "🦁 Lion's Project™",
   },
-}
+};
 
-export default config
+export default config;
