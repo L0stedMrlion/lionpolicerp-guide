3a69eda89d7d591251098025c720313e63413dfe
Mrlion <mrlion4202@gmail.com>
2024-05-17 13:47:24 +0000

    úprava barvy + přidáno logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,18 +4,31 @@ import { DocsThemeConfig } from "nextra-theme-docs";
 const config: DocsThemeConfig = {
   useNextSeoProps() {
     return {
-      titleTemplate: "%s",
+      titleTemplate: "%s - Guide",
     };
   },
   navigation: {
     prev: false,
     next: false,
   },
-  primaryHue: 222,
-  primarySaturation: 52,
+  primaryHue: 211,
+  primarySaturation: 100,
   docsRepositoryBase:
     "https://github.com/L0stedMrlion/lionpolicerp-guide/tree/main",
-  logo: <span>🦁 Lion Police Roleplay™</span>,
+  logo: (
+    <div
+      style={{
+        paddingLeft: "50px",
+        lineHeight: "38px",
+        background:
+          "url('https://cdn.discordapp.com/attachments/1178330264263462943/1229136630045081691/lionsproject_logo.png?ex=66484af0&is=6646f970&hm=6ff109822bd6782e7182b4c251a6788976e301198b7827b8e557d7e9c47c11ec&') no-repeat left",
+        backgroundSize: "38px",
+        fontWeight: 550,
+      }}
+    >
+      🦁 Lion Police Roleplay™
+    </div>
+  ),
   project: {
     link: "https://github.com/L0stedMrlion/lionpolicerp-guide",
   },
